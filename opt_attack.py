fdfc4540fd2b1f52d202a7d944871079b2533c64
ErichKramer <erich.kramer1@gmail.com>
2018-03-14 19:51:58 -0700

    WE can generate a filter, now let's view it

--- opt_attack.py
+++ opt_attack.py
@@ -13,6 +13,7 @@ import testModel
 import random
 import pdb
 
+from ProjectImageHandler import ProjectImageHandler as PM
 
 #need to load in test data
 
@@ -32,53 +33,49 @@ class optAttacker:
         #self.optim  = optim.SGD(params=[self.r], lr=.008)#params=[self.r?]
 
 
+
+    #mode is the possible classifications, e.g. output layer width as list
     #expects true_t
     def filterFit(self, x, true_y, goal_y=None, mode=range(10) ):
-        #mode is the possible classifications, e.g. output layer width as list
-        #init an empty filter
-        
+        #get goal, confirm its not predicted before running
+        if goal_y == None:
+            mode.pop(true_y)
+            goal_y = random.choice( mode )#list of classes\actual class
 
-        r = nn.Parameter( data=torch.zeros(1,28,28), requires_grad=True)
-        x_var = Variable(x) #assumes x is torch tensor
-        
+        r = nn.Parameter( data=torch.zeros(1,28,28).cuda(), requires_grad=True)
+        x_var = Variable(x.cuda()) #assumes x is torch tensor
+        y_tensor_target = Variable( torch.LongTensor( [ goal_y]).cuda() )
 
-        pdb.set_trace()
-        pred = torch.max( self.net( x_var.cuda() ).data, 1)
         #return 0 filter if you ask it to fit to current prediction
+        pred = torch.max( self.net( x_var.cuda() ).data, 1)
         if pred == goal_y:
             print("Warning: Goal is equivalent to predicted. This is not expected")
             return r
 
-        if goal_y == None:
-            mode.pop(true_y)
-            goal_y = random.choice( mode )#list of classes\actual class
-        y_tensor_target = Variable( torch.LongTensor( [ goal_y]) )
         
 
         opt = optim.SGD( params=[r], lr=.008)
-
+        self.optimizer = opt
 
         for i in range(1000):
-            print("Beginning to optimize filter, iter ", i)
             opt.zero_grad()
             tmp = x_var+r
-            tmp = tmp.cuda()
-            pdb.set_trace()
+            #tmp = tmp.cuda()
 
-            outputs = self.net( tmp[0] ) #net.forward if doesnt work
+            outputs = self.net( tmp ) #net.forward if doesnt work
             #loss is equal to the actual loss against the desired, and the magnitude of r`
             adv_loss = self.cross(outputs, y_tensor_target) + torch.mean(torch.pow( r, 2) )
             adv_loss.backward()
             self.optimizer.step()
             
-            pdb.set_trace()
             predicted = torch.max( outputs.data, 1)
 
             if int(predicted[1]) == goal_y:#grab index and cast to int
+                print("Filter optimized for {} iterations".format(i) )
                 break
         #before return
-        pdb.set_trace()
         
+
         return r
 
 if __name__ == "__main__":
@@ -96,13 +93,20 @@ if __name__ == "__main__":
     MNIST_LOADER = torch.utils.data.DataLoader(MNIST, batch_size=BATCH_SIZE, 
                     shuffle=False, num_workers=2)
     
-    test_acc = testModel.get_test_acc( atk.net, MNIST_LOADER)
-    print('test_acc %.5f' % (test_acc))
+    #test_acc = testModel.get_test_acc( atk.net, MNIST_LOADER)
+    #print('test_acc %.5f' % (test_acc))
 
     image,label = MNIST[0]
     
-    r = atk.filterFit(image, label)
+    r = atk.filterFit(image, label, 1)
+
+    #adv_exam = MNIST[0][0].cuda()+r
+
+    tmp = image.cuda() + r.data
+    manip = PM()
 
     pdb.set_trace()
+
+
 
 
